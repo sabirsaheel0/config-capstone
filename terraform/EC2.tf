51757df7bbf0df813294e8f6d5e39ac438ea9654
sabirsaheel0 <sabirsaheel17@gmail.com>
2026-03-28 00:36:43 +0000

    Updated terraform with SG and updated ansible

--- terraform/EC2.tf
+++ terraform/EC2.tf
@@ -2,7 +2,8 @@ resource "aws_instance" "bastion_host" {
   ami           = "ami-0ec10929233384c7f"
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.public-subnet-1.id
-
+  key_name = "jenkins"
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
   tags = {
     Name = "bastion_host"
   }
@@ -12,6 +13,8 @@ resource "aws_instance" "Jenkins_server" {
   ami           = "ami-0ec10929233384c7f"
   instance_type = "t3.small"
   subnet_id     = aws_subnet.public-subnet-1.id
+  key_name = "jenkins"
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
 
   tags = {
     Name = "Jenkins_server"
@@ -22,6 +25,8 @@ resource "aws_instance" "Sonarqube_server" {
   ami           = "ami-0ec10929233384c7f"
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.public-subnet-1.id
+  key_name = "jenkins"
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
 
   tags = {
     Name = "Sonarqube_server"
---
+++ terraform/security_groups.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group" "jenkins_sg" {
+  name   = "jenkins_sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Jenkins UI"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
